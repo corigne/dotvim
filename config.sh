b9da0be7f7c5cc83405be6a86655ef264119b777
Nathan Jodoin <nathan@Nathans-MacBook-Pro.local>
2022-09-24 17:23:25 -0500

    added configuration script

---
+++ config.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#Clone the vundle repo to the correct bundle location.
+cd ~
+mkdir .vim
+mkdir .vim/bundle/
+[ -e .vim/bundle/Vundle.vim ] && rm -Rf .vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+echo 'Vim should be configured now, mostly...'
+echo 'Check clang binary location is set correctly in clang_complete config in:'
+echo '.vim/bundle/clang_complete/plugin/clang_complete.vim'
