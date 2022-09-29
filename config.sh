5ad716fb539249787e6fbd0262d9e7631f7b12db
Nathan Jodoin <nathanjodoin@gmail.com>
2022-09-29 13:52:53 -0500

    added gitignore for vimrc in config.sh

--- config.sh
+++ config.sh
@@ -6,6 +6,9 @@ mkdir bundle
 [ -e .vim/bundle/Vundle.vim ] && rm -Rf .vim/bundle/Vundle.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+echo "Adding vimrc file to local .gitignore so changes made to the config aren't tracked..."
+git update-index --assume-unchanged vimrc
+
 echo 'Vim should be configured now, mostly...'
 echo 'Check clang binary location is set correctly in clang_complete config in:'
 echo '.vim/bundle/clang_complete/plugin/clang_complete.vim'
